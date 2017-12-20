06ed01a291b702f441c5db2d319bc54d12cd47ef
gori78 <32679520+gori78@users.noreply.github.com>
2017-12-20 08:56:03 +0100

    Update WCiOS.m
    - added NSLog for event activationDidCompleteWithState
    - changed sendMessage from transferUserInfo -> sendMessage

--- src/ios/WCiOS.m
+++ src/ios/WCiOS.m
@@ -25,19 +25,35 @@
 
 - (void)session:(WCSession *)session didReceiveMessage:(NSDictionary<NSString *, id> *)message replyHandler:(void(^)(NSDictionary<NSString *, id> *replyMessage))replyHandler {
     NSError *err;
+    
+    NSLog(@"received data: %@",message);
+    
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:message options:0 error:&err] encoding:NSUTF8StringEncoding]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.messageReceiver];
     dispatch_async(dispatch_get_main_queue(), ^{
         replyHandler([[NSDictionary alloc] initWithObjects:@[self.messageString?self.messageString:@""] forKeys:@[@"message"]]);
     });
 }
+
+- (void)session:(WCSession *)session activationDidCompleteWithState:(WCSessionActivationState)activationState error:(NSError *)error{
+    NSLog(@"activationDidComplete with status: %ld and Error %@ ", (long)activationState, error);
+}
+
 - (void)sendMessage:(CDVInvokedUrlCommand*)command {
     NSString* message = [[command arguments] objectAtIndex:0];
     if (message != nil) {
         self.messageString = message;
     }
+    
+    NSLog(@"send data: %@", message);
+    
     NSDictionary *messageDictionary = [[NSDictionary alloc] initWithObjects:@[message] forKeys:@[@"message"]];
-    [[WCSession defaultSession] transferUserInfo:messageDictionary];
+    
+    [[WCSession defaultSession] sendMessage:messageDictionary
+                               replyHandler:^(NSDictionary *reply) {}
+                               errorHandler:^(NSError *error) {}
+     ];
+    
 }
 
 @end
